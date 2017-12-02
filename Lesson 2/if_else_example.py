51c2b0444628d4f09051c8a7c1ecfa08f1ecd291
Vanguard90 <enesmil@gmail.com>
2017-12-02 18:48:15 +0100

    Finisheed Lesson 2! Pretty basic stuff. Only syntax changes compareed to Javascript.

---
+++ Lesson 2/if_else_example.py
@@ -0,0 +1,9 @@
+#If-else example
+name = 'Enes'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice.')
+elif age < 12:
+    print('You\'re not Alice.')
+elif age > 2000:
+    print('Good try vampire!')
---
+++ Lesson 2/while_example.py
@@ -0,0 +1,8 @@
+test = 0
+target = 5
+
+while test < target:
+    print('Test is lower than ' + str(target) + '! ' 'It\'s currently ' + str(test) + '!')
+    test = int(test) + 1
+
+print('Done!')
