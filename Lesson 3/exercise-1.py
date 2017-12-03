3621491189944c8cda3e05a27b969be1141c7c86
Vanguard90 <enesmil@gmail.com>
2017-12-03 14:36:12 +0100

    Lesson 3 done!

---
+++ Lesson 3/exercise-1.py
@@ -0,0 +1,6 @@
+import random
+import pyperclip
+
+test = random.randint(1,20)
+
+print(test)
---
+++ Lesson 3/exercise-2.py
@@ -0,0 +1,10 @@
+def hello(name):
+    print('Hi ' + name + '!')
+    print('Hello ' + name + '!')
+
+hello('Enes')
+
+def plusOne(number):
+    return number + 1
+
+print(plusOne(5))
