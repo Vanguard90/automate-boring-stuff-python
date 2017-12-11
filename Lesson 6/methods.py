ff9d78327d0081f35f8ae6c0d05ca4ff16b4064a
Vanguard90 <enesmil@gmail.com>
2017-12-11 21:24:08 +0100

    List methods examples.

---
+++ Lesson 6/methods.py
@@ -0,0 +1,17 @@
+spam = ['hello', 'hi',1,2]
+
+print(spam.index(1))
+
+spam.append('999')
+
+print(spam)
+
+spam.insert(1,'Enes')
+
+print(spam)
+
+spam.remove('Enes')
+
+print(spam)
+
+##spam.sort() is not supported between integer and string
